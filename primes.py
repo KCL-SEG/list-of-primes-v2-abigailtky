71eff9da5156fae823ab9fb60de15d687a9e6450
abigailtky <127849272+abigailtky@users.noreply.github.com>
2023-11-13 15:38:36 +0000

    Extend primes function to return prime numbers of frequency given.

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes given should be a positive number, ie be above 0.")
+
+    while len(list) < number_of_primes:
+        is_prime = all(num % prime != 0 for prime in list)
+        if is_prime:
+            list.append(num)
+        num += 1
+    
     return list
